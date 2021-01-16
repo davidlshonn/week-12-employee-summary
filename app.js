9a765f81c9aa92d8ccac0b306eb6cb76fcbe819f
David Shonn <davidlshonn@icloud.com>
2021-01-16 00:41:59 +0000

    manager prompts added

--- app.js
+++ app.js
@@ -12,6 +12,74 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+
+    function createManager() {
+        console.log("Please build your team");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "Whatt is your managers name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                } 
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is your manager's id?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\D*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    {
+                        return "Please enter a postitve number greater than zero";
+                    }
+                }
+            },    
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your manager's email?",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                            return true;
+                        }
+                        return "Please enter a valid email address?"
+                    }   
+            },
+            {
+                    type: "input",
+                    name: "officeNumber",
+                    message: "What is your manager's office number?",
+                    validate: answer => {
+                        const pass = answer.match(
+                            /^[1-9]\D*$/
+                        );
+                        if (pass) {
+                            return true;
+                        }
+                        {
+                            return "Please enter a postitve number greater than zero";
+                        }
+                    }
+            },
+        ])
+    }
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
